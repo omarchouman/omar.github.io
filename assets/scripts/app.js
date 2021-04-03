5bab8ec5c970301ba40ca6377977da0ecbdd4c71
Omar Chouman <omar.chouman@hotmail.com>
2021-04-03 16:53:18 +0300

    Made The Copyright-Year Dynamic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -74,6 +74,15 @@ const Portfolio = function() {
 
 }();
 
+window.addEventListener('load', (
+    function () {
+        document.getElementById("copyright-year").appendChild(
+            document.createTextNode(
+                new Date().getFullYear()
+            )
+        );
+    }
+));
 
 Portfolio.displayWordCloud();
 Portfolio.typeAnimation();
